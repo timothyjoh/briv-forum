74c1ca7ea2fdae8e89e6dc161a27021448324f40
Tim Johnson <jhnsntmthy@gmail.com>
2021-09-01 05:11:32 -0400

    bootstrap working

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'cloudinary'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'webpacker'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
---
+++ config/webpack/environment.js
@@ -0,0 +1,12 @@
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  JQuery: 'jquery',
+  jquery: 'jquery',
+  'window.Tether': "tether",
+  Popper: ['popper.js', 'default']
+}))
+
+module.exports = environment
